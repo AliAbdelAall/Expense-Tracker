b889d8b415d1bfe0e2d8c59220257b301f5ca7fe
AliAbdelAall <3laych@gmail.com>
2024-03-10 16:24:48 +0200

    added all nessessary ids to js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
+const total_balance = document.getElementById("total-balance")
+const total_income = document.getElementById("total-income")
+const total_expence = document.getElementById("total-expense")
+const filter_type = document.getElementById("filter-type")
+const filter_currency = document.getElementById("filter-currency")
+const amount_to = document.getElementById("amount-to")
+const amount_from = document.getElementById("amount-from")
+const add_btn = document.getElementById("add-btn")
+const add_type = document.getElementById("add-type")
+const add_currency = document.getElementById("add-currency")
+const add_amount = document.getElementById("add-amount")
+const add_input = document.getElementById("add-input")
+const transactions = document.getElementById("transactions")
 
 const currencies = fetch("https://crowded-cyan-wildebeest.cyclic.app/students/available")
 
