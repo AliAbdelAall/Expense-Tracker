3a274076e9eac2778d3fe8f5d3faab098cc0fb2e
AliAbdelAall <3laych@gmail.com>
2024-03-10 16:44:54 +0200

    added createElement function

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,36 @@ const add_amount = document.getElementById("add-amount")
 const add_input = document.getElementById("add-input")
 const transactions = document.getElementById("transactions")
 
+// function createTransation(type, classes, value) {
+//   const element1 = document.createElement(type)
+
+//   for (const className of classes) {
+//      element1.classList.add(className)
+//   }
+
+//   element1.innerText = value
+// }
+
+// function adTransaction() {
+//   const div = createTransation("div", ["flex, background-black", "justify-content"], "50")
+//   const p = createTransation("p", ["flex, background-black", "justify-content"], "50")
+
+//   div.appendChild(p)
+
+// }
+
+
+const createElement = (type, classes, value) => {
+  const element = document.createElement(type)
+
+  for (let i = 0; i < classes.length; i++) {
+    element.classList.add(classes[i])
+  }
+  if (value) {
+    element.innerText = value
+  }
+}
+
 const currencies = fetch("https://crowded-cyan-wildebeest.cyclic.app/students/available")
 
 currencies.then(response => response.json())
