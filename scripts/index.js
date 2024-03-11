3f7f52504e9d052d76ec1e6e59b9ea7ecb1bbd1d
AliAbdelAall <3laych@gmail.com>
2024-03-11 02:32:14 +0200

    added loadCurrencies function

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const total_balance = document.getElementById("total-balance")
 const total_income = document.getElementById("total-income")
 const total_expense = document.getElementById("total-expense")
 const filter_type = document.getElementById("filter-type")
-const filter_currency = document.getElementById("filter-currency")
+const filter_currency = document.getElementById("fliter-currency")
 const amount_to = document.getElementById("amount-to")
 const amount_from = document.getElementById("amount-from")
 const add_btn = document.getElementById("add_btn")
@@ -18,28 +18,6 @@ let total_income_update = parseFloat(total_income.innerText)
 let total_expense_update = parseFloat(total_expense.innerText)
 let total_balance_update = parseFloat(total_balance.innerText)
 
-function saveData() {
-  const data = {
-    transactions: transactions.innerHTML,
-    totalBalance: total_balance.innerText,
-    totalIncome: total_income.innerText,
-    totalExpense: total_expense.innerText
-  };
-  localStorage.setItem("expenseTrackerData", JSON.stringify(data));
-}
-
-function loadData() {
-  const savedData = JSON.parse(localStorage.getItem("expenseTrackerData"));
-  if (savedData) {
-    transactions.innerHTML = savedData.transactions;
-    total_balance.innerText = savedData.totalBalance;
-    total_income.innerText = savedData.totalIncome;
-    total_expense.innerText = savedData.totalExpense;
-  }
-}
-
-loadData()
-
 const result_currencies = fetch("https://rich-erin-angler-hem.cyclic.app/students/available")
 
 const getCurrencies = async () => {
@@ -47,7 +25,11 @@ const getCurrencies = async () => {
     const response = await result_currencies;
     const data = await response.json()
     currencies = data
-    console.log(currencies)
+    console.log(currencies, typeof (currencies))
+    if (data) {
+      loadCurrencies(filter_currency)
+      loadCurrencies(add_currency)
+    }
   } catch (error) {
     console.error("Failed to fetch currencies:", error)
   }
@@ -72,17 +54,51 @@ const convertCurrencies = async (cur, amount) => {
   }
 }
 
-const createElement = (type, classes, value) => {
+const createElement = (type, classes, inner_text, value = null) => {
   const element = document.createElement(type)
 
   for (let i = 0; i < classes.length; i++) {
     element.classList.add(classes[i])
   }
+  if (inner_text) {
+    element.innerText = inner_text
+  }
   if (value) {
-    element.innerText = value
+    element.value = value
   }
   return element
 }
+
+
+const loadCurrencies = (element) => {
+  currencies.forEach((currency) => {
+    const { code } = currency
+    const option_ele = createElement("option", [], code, code)
+    element.appendChild(option_ele)
+  })
+}
+
+const saveData = () => {
+  const data = {
+    transactions: transactions.innerHTML,
+    totalBalance: total_balance.innerText,
+    totalIncome: total_income.innerText,
+    totalExpense: total_expense.innerText
+  };
+  localStorage.setItem("expenseTrackerData", JSON.stringify(data));
+}
+
+const loadData = () => {
+  const savedData = JSON.parse(localStorage.getItem("expenseTrackerData"));
+  if (savedData) {
+    transactions.innerHTML = savedData.transactions;
+    total_balance.innerText = savedData.totalBalance;
+    total_income.innerText = savedData.totalIncome;
+    total_expense.innerText = savedData.totalExpense;
+  }
+}
+
+loadData()
 
 const updateTotalBalance = async (type, amount_ele, currency_ele) => {
   console.log(total_income_update, typeof (total_income_update))
@@ -103,21 +119,21 @@ const updateTotalBalance = async (type, amount_ele, currency_ele) => {
 }
 
 const createTransaction = (type, amount, currency, info) => {
-  const transactionId = `transaction-${Date.now()}`
-  const amountId = `amount-${Date.now()}`
+  const transaction_id = `transaction-${Date.now()}`
+  const amount_id = `amount-${Date.now()}`
   if (!amount || !info) {
     input_handler.classList.remove("hidden")
     return
   }
   input_handler.classList.add("hidden")
   const outer_div = createElement("div", ["flex", "align-center", "dark-grey-background", "padding-7-15", "radius-10", "space-between", "transaction"], null)
-  outer_div.id = transactionId
+  outer_div.id = transaction_id
   const info_div = createElement("div", ["flex", "gap-10", "transaction-info", "white"], null)
   const edit_delete_div = createElement("div", ["flex", "wrap", "gap-10", "edit-delete"], null)
   const amount_currency_div = createElement("div", [], null)
   const type_ele = createElement("i", ["fa-solid", `${type === "expense" ? "fa-arrow-down" : "fa-arrow-up"}`], null)
   const amount_ele = createElement("span", ["amount"], `${type === "expense" ? `${- amount}` : `${amount}`}`)
-  amount_ele.id = amountId
+  amount_ele.id = amount_id
   const currency_ele = createElement("span", ["currency"], currency)
   const description = createElement("span", ["info"], info)
   const edit_ele = createElement("button", ["bg-primary-color", "padding-7-15", "radius-10", "no-bor", "no-outln", "white", "edit"], "Edit")
